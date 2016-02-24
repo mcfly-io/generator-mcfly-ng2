2703efa37a2f11010b9ded1c095ddc27be1dbf99
Avi Haiat <thaiat@ipelia.fr>
2016-02-24 23:40:06 +0100

    fix(app): Fix issue with cast in unit test

--- generators/component/templates/_component.spec.ts
+++ generators/component/templates/_component.spec.ts
@@ -11,7 +11,7 @@ describe('Component: <%=componentnameClass%>Component', () => {
         return tcb.createAsync(<%=componentnameClass%>Component)
             .then((fixture) => {            
                 let element = fixture.debugElement.nativeElement;
-                let cmpInstance = <<%=componentnameClass%>>fixture.debugElement.componentInstance;
+                let cmpInstance = <<%=componentnameClass%>Component>fixture.debugElement.componentInstance;
                 fixture.detectChanges();
 
                 expect(cmpInstance).toBeDefined();
